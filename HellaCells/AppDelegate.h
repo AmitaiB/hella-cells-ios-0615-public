71c8acd23b89944f24392cf500cad32022bafea4
AmitaiB <blickstein@gmail.com>
2015-06-16 19:56:53 -0400

    sets up the lab

--- HellaCells/AppDelegate.h
+++ HellaCells/AppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FISHellaMasterTableViewController.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
---
+++ HellaCells/FISHellaMasterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISHellaMasterTableViewController.h
+//  HellaCells
+//
+//  Created by Amitai Blickstein on 6/16/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaMasterTableViewController : UITableViewController
+
+@end
